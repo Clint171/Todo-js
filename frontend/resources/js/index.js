e1375d00f97a8e558996bbb111ab7f0b79a82c5b
Clint171 <clintsimiyu004@gmail.com>
2023-10-17 14:45:41 +0300

    feat:added overdue red function

--- frontend/resources/js/index.js
+++ frontend/resources/js/index.js
@@ -7,80 +7,48 @@ let newTaskForm = document.getElementById("newTaskForm");
 let container = document.getElementById("container");
 
 popup();
+//localStorage.clear();
 if(localStorage.getItem("tasks")){
     taskArray = JSON.parse(localStorage.getItem("tasks"));
 }
-displayTasks();
-
-function displayTasks(){
-    hideNav();
-    var items = document.getElementsByClassName("item");
-    for(i = 0 ; i < items.length ; i++){
-    container.removeChild(items[i]);
+taskArray.forEach( task =>{
+    if(task.date.toDateString > Date.now()){
+        if(task.completed == false){
+            alert("tasks are overdue");
+            task.overdue = true;
+        }
     }
-
-taskArray.forEach(task => {
-    var itemDiv = document.createElement("div");
-    var itemTitle = document.createElement("h2");
-    var titleSpan = document.createElement("span");
-    var check = document.createElement("input");
-    var p = document.createElement("p");
-    var pDate = document.createElement("span");
-    var pTime = document.createElement("span");
-    itemDiv.classList.add("item");;
-    itemTitle.classList.add("item-title");
-    check.classList.add("item-check");
-    check.setAttribute("type" , "checkbox");
-    check.setAttribute("onclick" , `updateTask(${task.id})`);
-    if(task.completed == true){
-        itemTitle.classList.add("item-title-completed");
-        check.setAttribute("checked" , "");
-        check.classList.add("item-check-clicked");
-    }
-    else{
-        check.removeAttribute("checked");
-        check.classList.remove("item-check-clicked");
-        itemTitle.classList.remove("item-title-completed");
-    }
-    p.classList.add("item-description");
-
-    titleSpan.textContent = task.title;
-    pDate.textContent = task.date.split("T")[0];
-    pTime.textContent = task.date.split("T")[1];
-
-    container.appendChild(itemDiv);
-    itemDiv.appendChild(itemTitle);
-    itemDiv.appendChild(p);
-    itemTitle.appendChild(titleSpan);
-    itemTitle.appendChild(check);
-    p.appendChild(pDate);
-    p.appendChild(pTime);
-
 });
+
+
+displayAllTasks();
+
+function displayAllTasks(){
+    clearTaskScreen();
+    hideNav();
+    displayTasks(taskArray);
 }
 
 function displayPendingTasks(){
-    displayTasks();
+    clearTaskScreen();
     hideNav();
-    var items = document.getElementsByClassName("item");
-    for(i = 0 ; i < items.length ; i++){
-        if(taskArray[i].completed == true)
-            container.removeChild(items[i]);
-    }
+    displayTasks(returnPendingTasks(taskArray));
 }
 function displayCompletedTasks(){
+    clearTaskScreen();
     hideNav();
-    displayTasks();
-    var items = document.getElementsByClassName("item");
-    for(i = 0 ; i < items.length ; i++){
-        if(taskArray[i].completed == false)
-            container.removeChild(items[i]);
-    }
+    displayTasks(returnCompletedTasks(taskArray));
+}
+function displayOverdueTasks(){
+    clearTaskScreen();
+    hideNav();
+    displayTasks(returnOverdueTasks(taskArray));
 }
 
 
 function showNav(){
     hideAllScreens();
+    newTaskForm.reset();
     nav.classList.remove("hidden");
     navIcon.setAttribute("onclick" ,"hideNav()");
     addIcon.classList.add("hidden");
@@ -115,17 +83,18 @@ function hideAllScreens(){
 }
 
 newTaskForm.addEventListener("submit", (event)=>{
-
+    popup();
     event.preventDefault();
     var title = document.getElementById("title").value;
     var description = document.getElementById("description").value;
-    var date = document.getElementById("date").value;
-    popup();
+    let date = document.getElementById("date").value;
+    date = new Date(date);
     var task = new Task(taskArray.length , title , description , date);
     taskArray.push(task);
     localStorage.setItem("tasks" , JSON.stringify(taskArray));
     hideScreen('newTaskScreen');
-    displayTasks();
+    displayAllTasks();
+    newTaskForm.reset();
 });
 
 class Task{
@@ -164,5 +133,96 @@ function updateTask(id){
     else{
         taskArray[id].completed = false;
     }
-    displayTasks();
+    localStorage.setItem("tasks" , JSON.stringify(taskArray));
+    displayAllTasks();
+}
+
+function showDescription(id){
+    var p = document.createElement("p");
+    p.setAttribute("id" , i);
+    p.textContent = taskArray[id].description;
+    p.style.width = "100%";
+    document.getElementById(id).appendChild(p);
+}
+
+//Create function to display tasks that takes the array of tasks as a parameter
+function displayTasks(tasks){
+
+    tasks.forEach(task => {
+        var itemDiv = document.createElement("div");
+        var itemTitle = document.createElement("h2");
+        var titleSpan = document.createElement("span");
+        var check = document.createElement("input");
+        var p = document.createElement("p");
+        var pDate = document.createElement("span");
+        var pTime = document.createElement("span");
+        itemDiv.classList.add("item");;
+        itemTitle.classList.add("item-title");
+        check.classList.add("item-check");
+        check.setAttribute("type" , "checkbox");
+        check.setAttribute("onclick" , `updateTask(${task.id})`);
+        if(task.overdue == true){
+            if(task.completed == false){
+                itemDiv.classList.add("overdue");
+            }
+        }
+        if(task.completed == true){
+            itemTitle.classList.add("item-title-completed");
+            check.setAttribute("checked" , "");
+            check.classList.add("item-check-clicked");
+        }
+        else{
+            check.removeAttribute("checked");
+            check.classList.remove("item-check-clicked");
+            itemTitle.classList.remove("item-title-completed");
+        }
+        p.classList.add("item-description");
+    
+        titleSpan.textContent = task.title;
+        pDate.textContent = task.date.split("T")[0];
+        pTime.textContent = task.date.split("T")[1].split(".")[0];
+    
+        container.appendChild(itemDiv);
+        itemDiv.appendChild(itemTitle);
+        itemDiv.appendChild(p);
+        itemTitle.appendChild(titleSpan);
+        itemTitle.appendChild(check);
+        p.appendChild(pDate);
+        p.appendChild(pTime);
+    });
+
+}
+function returnPendingTasks(tasks){
+    var pendingTasks = [];
+    tasks.forEach(task => {
+        if(task.completed == false){
+            pendingTasks.push(task);
+        }
+    });
+    return pendingTasks;
+}
+
+function returnCompletedTasks(tasks){
+    var completedTasks = [];
+    tasks.forEach(task => {
+        if(task.completed == true){
+            completedTasks.push(task);
+        }
+    });
+    return completedTasks;
+}
+function returnOverdueTasks(tasks){
+    var overdueTasks = [];
+    tasks.forEach(task => {
+        if(task.overdue == true){
+            if(task.completed == false){
+                overdueTasks.push(task);
+            }
+        }
+    });
+    return overdueTasks;
+}
+
+function clearTaskScreen(){
+    container.innerHTML = "";
 }
