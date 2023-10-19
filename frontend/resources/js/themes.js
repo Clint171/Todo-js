fba268a8b7829c09807cf4092e6ea70eba740634
Clint171 <clintsimiyu004@gmail.com>
2023-10-19 16:09:29 +0300

    chore: cleaned up code, removed comments

--- frontend/resources/js/themes.js
+++ frontend/resources/js/themes.js
@@ -1,10 +1,9 @@
 let themeSelector = document.getElementById("theme-selector");
 let body = document.querySelector("body");
 let header = document.getElementById("header");
-let items = document.querySelectorAll(".item");
+let items = document.getElementsByClassName("item");
 let screens = document.querySelectorAll(".screen");
 let spans = document.querySelectorAll("span");
-
 themeSelector.value = localStorage.getItem("theme");
 changeTheme(themeSelector.value);
 
@@ -16,24 +15,54 @@ themeSelector.addEventListener("click", function(){
     changeTheme(themeSelector.value);
     localStorage.setItem("theme", themeSelector.value);
 });
-
 function changeTheme(themeColor){
     header.style.backgroundColor = themeColor;
     body.style.backgroundColor = themeColor;
     popupDiv.style.backgroundColor = themeColor;
     nav.style.backgroundColor = themeColor;
 
+    let rgb = hexToRgb(themeColor);
+    let r = rgb[0];
+    let g = rgb[1];
+    let b = rgb[2];
+
     for(let i = 0; i < items.length; i++){
-        //Item color should be lighter version of themecolor
-        items[i].style.backgroundColor = themeColor;
+        items[i].style.backgroundColor = `rgb(${r+50},${g+50},${b+50})`;
     }
     for(let i = 0; i < screens.length; i++){
-        //Item color should be lighter version of themecolor
-        screens[i].style.backgroundColor = themeColor;
+        screens[i].style.backgroundColor = `rgb(${r+50},${g+50},${b+50})`;
     }
     for(let i = 0; i < spans.length; i++){
-        //Item color should be lighter version of themecolor
-        spans[i].style.backgroundColor = themeColor;
+        spans[i].style.backgroundColor = `rgb(${r+50},${g+50},${b+50})`;
+    }
+    //Check if rgb values are below 128
+    if(r < 100 && g < 100 && b < 100){
+        //Change text color to white
+        body.style.color = "#FFFFFF";
+        document.querySelector("path").style.fill = "#FFFFFF";
+    }
+    else{
+        //Change text color to black
+        body.style.color = "#000000";
+        document.querySelector("path").style.fill = "#000000";
     }
     displayAllTasks();
+}
+function hexToRgb(hex) {
+    // Convert the hex value to an RGB tuple.
+    const rgb = [];
+    hex = hex.replace("#", "");
+    for (let i = 0; i < 3; i++) {
+      rgb.push(parseInt(hex.substring(i * 2, i * 2 + 2), 16));
+    }
+    return rgb;
+}
+  
+  function rgbToHex(rgb) {
+    // Convert the RGB tuple to a hex value.
+    const hex = '#';
+    for (const color of rgb) {
+      hex += color.toString(16).padStart(2, '0');
+    }
+    return hex;
 }
