bd781f04cf93b9a9197ebbcf3c859988a729372e
Clint171 <clintsimiyu004@gmail.com>
2023-11-11 16:56:17 +0300

    feat : configured session

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require("./lib/routers/router.js");
 const dotenv = require("dotenv");
+const session = require("express-session");
 const User = require("../schemas/user.js");
 const Task = require("../schemas/task.js");
 const url = process.env.MONGO_URL;
@@ -9,12 +10,21 @@ mongoose.connect(url , {
     dbName : "Todo-js"
 });
 
+//Set up session store
+const MongoStore = require("connect-mongo")(session);
+const store = new MongoStore({
+    mongooseConnection : mongoose.connection,
+    collection : "sessions"
+});
+
 dotenv.config();
 
 const app = express();
 
 let port = process.env.PORT || 3500;
 
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 app.use(router);
 
 app.listen(port , ()=>{
