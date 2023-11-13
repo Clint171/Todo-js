c652665c161a666d8181039da50f17efb93fa082
Clint171 <clintsimiyu004@gmail.com>
2023-11-13 18:56:16 +0300

    fix: fixed signup

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
 const express = require("express");
-const router = require("./lib/routers/router.js");
+//const router = require("./lib/routers/router.js");
+const userRouter = require("./lib/routers/userRouter.js");
 const dotenv = require("dotenv");
-const session = require("express-session");
-const User = require("../schemas/user.js");
-const Task = require("../schemas/task.js");
-const url = process.env.MONGO_URL;
-
-mongoose.connect(url , {
-    dbName : "Todo-js"
-});
-
-//Set up session store
-const MongoStore = require("connect-mongo")(session);
-const store = new MongoStore({
-    mongooseConnection : mongoose.connection,
-    collection : "sessions"
-});
 
 dotenv.config();
 
@@ -25,7 +11,7 @@ let port = process.env.PORT || 3500;
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
-app.use(router);
+app.use(userRouter);
 
 app.listen(port , ()=>{
     console.log(`Server listening on port ${port}`);
