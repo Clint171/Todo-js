028c9cfa8c732aaec9116c9347767cf452d0a71e
Clint171 <clintsimiyu004@gmail.com>
2023-11-15 11:37:48 +0300

    fix: fixed email verification

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ let port = process.env.PORT || 3500;
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
-app.use(userRouter);
+app.use(signupRouter);
+app.use(loginRouter);
 
 app.listen(port , ()=>{
     console.log(`Server listening on port ${port}`);
