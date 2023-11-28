905a0df159cda5723434c2ef19cc838b9c26a879
Clint171 <clintsimiyu004@gmail.com>
2023-11-28 14:57:09 +0300

    chore[REFACTOR]: changed directory structure

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const express = require("express");
 //const router = require("./lib/routers/router.js");
 const db = require("./lib/db/db.js");
-const signupRouter = require("./lib/routers/signupRouter.js");
-const loginRouter = require("./lib/routers/loginRouter.js");
-const taskRouter = require("./lib/routers/taskRouter.js");
+const signupRouter = require("./lib/routers/user/signupRouter.js");
+const loginRouter = require("./lib/routers/user/loginRouter.js");
+const taskRouter = require("./lib/routers/task/taskRouter.js");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -17,7 +17,6 @@ app.use(express.urlencoded({extended : true}));
 app.use(signupRouter);
 app.use(loginRouter);
 app.use("/tasks" , taskRouter);
-//app.use(loginRouter);
 
 app.listen(port , ()=>{
     console.log(`Server listening on port ${port}`);
