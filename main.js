2bcb4cd77151f64f387e405b8a329c9dec874cd5
Clint171 <clintsimiyu004@gmail.com>
2023-12-04 18:22:51 +0300

    fix: changed port back to 3000

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-let port = process.env.PORT || 3500;
+let port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
